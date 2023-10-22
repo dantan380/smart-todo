ead370d49be1a13d1975ebe694595d1c8ab037d0
Garrick Henne <garrickhenne@outlook.com>
2023-10-21 17:54:35 -0700

    Add UPDATE query for category with todo id

--- db/queries/todos.js
+++ db/queries/todos.js
@@ -3,8 +3,26 @@ const db = require('../connection');
 const getTodosById = (id) => {
   return db.query('SELECT * FROM todos WHERE user_id = $1', [id])
     .then(todos => {
-      return todos.rows
+      return todos.rows;
     });
 };
 
-module.exports = { getTodosById };
+const updateCategoryWithId = (newCategory, todoId) => {
+  // First get category id of new category.
+  return db.query(`SELECT id FROM categories WHERE name = $1 LIMIT 1`, [newCategory])
+    .then(res => {
+      if (!res.rows[0]) {
+        throw Error('No category id found from category name.');
+      }
+      return res.rows[0];
+    })
+    // Then update user's todo with category id.
+    .then(categoryId => db.query(`UPDATE todos SET category_id = $1 WHERE id = $2 RETURNING *`, [categoryId, todoId]))
+    .then(res => res.rows)
+    .catch(err => {
+      console.log('Error occurred:', err.message);
+      throw err;
+    });
+};
+
+module.exports = { getTodosById, updateCategoryWithId };
