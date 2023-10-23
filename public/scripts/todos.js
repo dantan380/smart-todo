ae081c78c7ff0c1713418a79d8f93e7dbf7d63d8
Garrick Henne <garrickhenne@outlook.com>
2023-10-23 11:11:50 -0700

    Add error handling change UI

--- public/scripts/todos.js
+++ public/scripts/todos.js
@@ -1,4 +1,6 @@
 const MAX_TITLE_LENGTH = 255;
+// 2 Seconds.
+const DEFAULT_WAIT_TIME = 2000;
 
 /* eslint-disable no-undef */
 $(function() {
@@ -10,21 +12,46 @@ const enterNewToDo = function(event) {
   //stop the submission from our HTML form
   event.preventDefault();
 
-  const newTodoString = $(event.target).find('textarea').val();
+  const $newTodo = $(event.target).find('textarea');
+  const newTodoString = $newTodo.val();
 
   if (newTodoString === '') {
     console.log("😓 The To Do is empty.");
+    formError($newTodo, 'Please input a todo!');
     return;
   }
 
   if (newTodoString.length > MAX_TITLE_LENGTH) {
     console.log('Todo title too large!');
+    // Reset title text.
+    $newTodo.val('');
+    formError($newTodo, 'Your title is too long!');
     return;
   }
 
   // Send POST method to '/api/todos' URL with data as JSON.
   console.log('Submitting text: ', newTodoString);
   $.ajax({ method: 'POST', url: '/api/todos', data: { text: newTodoString } });
+  // TODO: Might have to add then,catch depending on what response gets sent back.
+};
+
+const formError = ($element, errMessage) => {
+  const oldClass = 'border border-gray-300';
+  const originalPlaceholder = $element.attr('placeholder');
+
+  // Make border thicker, change border color to red, and make placeholder text red.
+  const errorClasses = 'border-2 border-red-400 placeholder-red-400';
+
+  $element.removeClass(oldClass);
+  $element.addClass(errorClasses);
+  $element.attr('placeholder', errMessage);
+
+  // Set input field back to normal after delay.
+  setTimeout(() => {
+    $element.removeClass(errorClasses);
+    $element.addClass(oldClass);
+    $element.attr('placeholder', originalPlaceholder);
+  }, DEFAULT_WAIT_TIME);
 };
 
 
