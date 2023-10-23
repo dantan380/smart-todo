63c32a9a5a6e0f33bc7bd5a75e62328a994c9b1c
Garrick Henne <garrickhenne@outlook.com>
2023-10-22 22:55:06 -0700

    Update submit handler for new todo list entry

--- public/scripts/todos.js
+++ public/scripts/todos.js
@@ -1,50 +1,24 @@
-$(document).ready(function() {
+/* eslint-disable no-undef */
+$(function() {
   //handle submit events with AJAX
   $("#new-todo-form").on("submit", enterNewToDo);
-
 });
 
 const enterNewToDo = function(event) {
-
   //stop the submission from our HTML form
   event.preventDefault();
 
-  console.log("user entered new to do in text box");
+  const newTodoString = $(event.target).find('textarea').val();
 
-  const formData = $(event.target).serialize();
-
-  // Check if specific input fields are empty by...
-  //...filter out the "text=" (key)
-  //...check if the (value) is empty
-  const value = formData.slice(5);
-  if (value === "") {
-
+  if (newTodoString === '') {
     console.log("😓 The To Do is empty.");
     return;
   }
 
-  //attempt to make a post call
-  //TODO
+  // Send POST method to '/api/todos' URL with data as JSON.
+  console.log('Submitting text: ', newTodoString);
+  $.ajax({ method: 'POST', url: '/api/todos', data: { text: newTodoString } });
 };
-
-
-// Client facing scripts here
-$(() => {
-  $('#fetch-users').on('click', () => {
-    $.ajax({
-      method: 'GET',
-      url: '/api/users'
-    })
-      .done((response) => {
-        const $usersList = $('#users');
-        $usersList.empty();
-
-        for (const user of response.users) {
-          $(`<li class="user">`).text(user.name).appendTo($usersList);
-        }
-      });
-  });
-});
 
 
 
