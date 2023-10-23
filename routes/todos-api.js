1cc44b852afb257548b90c44b274f1b544f79f85
Daniel Tan <danieltan380@gmail.com>
2023-10-23 12:31:02 -0700

    Add GET route for all todos by ID

--- routes/todos-api.js
+++ routes/todos-api.js
@@ -13,6 +13,16 @@ const todoQueries = require('../db/queries/todos');
 //   next();
 // });
 
+router.get('/', (req, res) => {
+  todoQueries.getTodosById(1)
+  .then(todos => res.json(todos))
+  .catch(err => {
+    res
+      .status(401)
+      .json({ error: err.message });
+  });
+});
+
 router.patch('/:id', (req, res) => {
   // TODO: Get user's id from cookie
   todoQueries.updateCategoryWithId()
