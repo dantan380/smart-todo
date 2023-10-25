42b1d4bc307a07aaa7558f48f68147c856094b35
Garrick Henne <garrickhenne@outlook.com>
2023-10-25 14:14:58 -0700

    Update handler to handle check-mark-circle class

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,12 +1,12 @@
 // Client facing scripts here
-$(function () {
+$(function() {
   //...init page for user
 
   //load the user's categories here:
   loadUserTodos();
 });
 
-const loadUserTodos = function () {
+const loadUserTodos = function() {
   console.log("app.js: Loading user's to dos");
 
   $.ajax({ method: "GET", url: "/api/todos/withCategories", dataType: "json" })
@@ -19,7 +19,7 @@ const loadUserTodos = function () {
     });
 };
 
-const processTodos = function (todos) {
+const processTodos = function(todos) {
   //helpers = global object of helper functions (client-helper.js)
   clientHelper.locateCategoryElements();
   clientHelper.clearAllTodos();
@@ -31,12 +31,12 @@ const processTodos = function (todos) {
     //finally add the entry to the category.
     clientHelper.appendTodoOnCategory(element.name, $entry);
 
-    $entry.find("input").on("change", (event) => {
+    $entry.find(".checkmark-circle").on("click", (event) => {
       const todoId = $(event.currentTarget.parentElement).attr("data");
-      $.ajax({ method: "POST", url: "/api/todos/update", dataType: "json", data: {id: todoId, isCompleted: true}})
+      $.ajax({ method: "POST", url: "/api/todos/update", dataType: "json", data: { id: todoId, isCompleted: true } })
         .then(() => {
           $entry.remove();
-        })  
+        });
     });
   });
 };
